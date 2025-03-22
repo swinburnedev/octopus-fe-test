5096fd50c5be2eb866a08476875a710a4bdf9297
Andy Swinburne <andy@swinburne.dev>
2025-03-22 12:47:52 +0000

    add graphql query and basic page

--- client/pages/product.tsx
+++ client/pages/product.tsx
@@ -1,3 +1,89 @@
-export default function Product() {
-  return <div>Product page</div>;
+import type {InferGetServerSidePropsType, GetServerSideProps} from "next"
+import Head from "next/head"
+
+type Product = {
+    id: number
+    name: string
+    power: string
+    description: string
+    price: number
+    quantity: number
+    brand: string
+    weight: number
+    height: number
+    width: number
+    length: number
+    model_code: string
+    colour: string
+    img_url: string
+}
+
+export const getServerSideProps = (async () => {
+    const res = await fetch("http://localhost:3001/graphql", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            query: `{
+              Product(id: 1) {
+                id,
+                name,
+                power,
+                description,
+                price,
+                quantity,
+                brand,
+                weight,
+                height,
+                width,
+                length,
+                model_code,
+                colour,
+                img_url,
+              }
+            }`,
+        }),
+    })
+
+    const data = await res.json()
+    const product = data.data.Product
+    // TODO add not found handling
+    return {props: {product}}
+}) as GetServerSideProps<{product: Product}>
+
+export default function Product({product}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+    const {
+        id,
+        name,
+        power,
+        description,
+        price,
+        quantity,
+        brand,
+        weight,
+        height,
+        width,
+        length,
+        model_code,
+        colour,
+        img_url,
+    } = product || {}
+    return (
+        <>
+            <Head>
+                <title>{name}</title>
+                <meta name="description" content={description} />
+            </Head>
+            <section>
+                <h1>{name}</h1>
+                <span>
+                    {power} // Packet of {quantity}
+                </span>
+                <h2>Description</h2>
+                <p>{description}</p>
+                <h3>Specifications</h3>
+            </section>
+        </>
+    )
 }
